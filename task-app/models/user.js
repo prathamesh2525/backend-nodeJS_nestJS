473ff2025e49dccc7da8885eafca3a333b68aa66
Prathamesh Madniwale <61145586+prathamesh2525@users.noreply.github.com>
2022-11-08 12:25:47 +0530

    Create user.js

---
+++ task-app/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+
+const validator = require("validator")
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid")
+      }
+    },
+  },
+  password: {
+    type: String,
+    minlength: 8,
+    validate(value) {
+      if (value.toLowerCase().includes("pass")) {
+        throw new Error("Password cannot contain pass")
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+  },
+})
+
+
+module.exports = User
